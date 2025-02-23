6f55222d8af95526029f3f783d5304360ef9c1e7
LucasMenezes9 <llucas009@yahoo.com.br>
2025-02-22 21:41:38 -0300

    22/02/2025

---
+++ Aula3/Aula3_2.js
@@ -0,0 +1,22 @@
+/* 
+  If & Else -> Controlador de fluxo
+
+  if (se)
+  Else (se não)
+
+  Operadores de Comparação
+   > maior que
+   < menor que
+   == igual que
+   === igual que
+   >= maior ou igual que
+   <= menor ou igual que
+
+*/
+const notaAluno = 5
+
+if (notaAluno == 10){
+    console.log("Aluno Nota 10")
+} else {
+    console.log("Aluno Meia Boca")
+}
